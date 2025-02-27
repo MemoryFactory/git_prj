18e2e5620d1c323e9c9293eeb6ea50b27885a791
LZH(VM Linux) <MemoryFactory@163.com>
2025-02-27 17:24:05 +0800

    add command display in start.sh and finish.sh, fix minor errors in finish.sh

--- finish.sh
+++ finish.sh
@@ -5,28 +5,34 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo ": Do you commit all of your modifications (y/[n])?"
 read -p "> " answer
-if [ "$answer" != "y" || "$answer" != "yes" ]; then
-	exit 0
-else
+if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
 	echo ": OK"
+else
+	echo ": Exit"
+	exit 0
+fi
 
 echo ": Finish all your job!" 
 #1. switch to main
 if [ "$branch" != "main" ]; then
+	echo "> git switch main"
 	git switch main
 fi
 #2. merge dev
 if git rev-parse --quiet --verify dev; then
+	echo "> git merge --no-ff dev"
 	git merge --no-ff dev
 fi
 #3. push new files online
+echo "> git push $github main"
 while ! git push $github main; do
 	echo ": Push $github failed, retrying in 5 seconds..."
 	sleep 5
 done
+echo "> git push $gitee main"
 while ! git push $gitee main; do
 	echo ": Push $gitee failed, retrying in 5 seconds..."
 	sleep 5
 done
 
-echo ": Everything are done"
+echo ": Everything has been done"
--- start.sh
+++ start.sh
@@ -6,24 +6,30 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 echo ": Start a great day!"
 #1. switch to main
 if [ "$branch" != "main" ]; then
+	echo "> git switch main"
 	git switch main
 fi
 #2. pull new files online
+echo "> git pull $github main"
 while ! git pull $github main; do
 	echo ": Pull $github failed, retrying in 5 seconds..."
 	sleep 5
 done
+echo "> git pull $gitee main"
 while ! git pull $gitee main; do
 	echo ": Pull $gitee failed, retrying in 5 seconds..."
 	sleep 5
 done
 #3. switch to dev
 if git rev-parse --quiet --verify dev; then
+	echo "> git switch dev"
 	git switch dev
 else
+	echo "> git switch -c dev"
 	git switch -c dev
 fi
 #4. merge main
+echo "> git merge main"
 git merge main
 
 echo ": Working enviroment is prepared"
