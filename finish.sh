516b5caa0fe6b10ea8135efd931e05d3db9e3cdd
LZH(VM Linux) <MemoryFactory@163.com>
2025-02-28 10:17:23 +0800

    check uncommitted files in finish.sh

--- finish.sh
+++ finish.sh
@@ -3,13 +3,23 @@ github="origin"
 gitee="gitee"
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo ": Do you commit all of your modifications (y/[n])?"
-read -p "> " answer
-if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
-	echo ": OK"
-else
-	echo ": Exit"
+#echo ": Do you commit all of your modifications (y/[n])?"
+#read -p "> " answer
+#if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+#	echo ": OK"
+#else
+#	echo ": Exit"
+#	exit 0
+#fi
+
+if [[ $(git status --porcelain) ]]; then
+	echo -e "\e[93mWarning: There are uncommitted files!\e[0m"
+	git status --short
+	echo ": Commit everything before finishing your work"
+	echo -e "\e[5m>>>\e[0m \e[1;91mSTOP\e[0m \e[5m<<<\e[0m"
 	exit 0
+else
+	echo "> "
 fi
 
 echo ": Finish all your job!" 
